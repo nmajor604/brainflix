5f7b5f51193c4238d8c3e65448db0c3c0628b401
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-05-03 21:19:15 -0700

    Initialized basic html structure, main components, three git branches

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+      <div className="Main">
+        Main
+      </div>
+    );
+  }
+  
+  export default Main;
+  
---
+++ src/components/NextVideos.jsx
@@ -0,0 +1,10 @@
+function NextVideos() {
+    return (
+      <div className="next-videos">
+          Next Videos
+      </div>
+    );
+  }
+  
+  export default NextVideos;
+  
